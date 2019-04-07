19a2b4ce7b53a784a4ae0a43fa2c7985802092ab
Phillip Phan <phillip.phan12@yahoo.com>
2019-04-07 15:21:58 -0400

    Added Random API Call

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from "axios";
+//Allow - Control - Allow - Origin: *
 class App extends Component {
-  render() {
+    doSearch = () => {
+        let x = Math.random();
+        let num = Math.round(x*100);
+        console.log(num);
+        let link = "https://www.anapioficeandfire.com/api/characters/"
+        link += num;
+        console.log(link);
+        axios
+            .get(link)
+            .then(response => {
+                console.log(response.data);
+                //console.log(response.data.collection.items[3].links[0].href);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+    render() {
+        console.log('HI');
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" />
+                <button onClick={() => this.doSearch()}> Add to database
+                </button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
+
           </p>
           <a
             className="App-link"
